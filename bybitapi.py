d913e2b96afeeb040bec21d306cc19184f1fef8b
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-04-14 22:02:23 +0200

    fixes

--- bybitapi.py
+++ bybitapi.py
@@ -6,5 +6,6 @@ API_SECRET = config("API_SECRET")
 
 try:
     client = bybit.bybit(test=False, api_key=API_KEY, api_secret=API_SECRET)
+    print("Logged in successfully!")
 except Exception as e:
     print(f'Something went wrong while logging => {e}')
