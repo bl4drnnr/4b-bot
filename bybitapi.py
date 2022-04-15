313395682c131b08c4cae1cecb560438f367cc3c
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-04-15 17:26:19 +0200

    error handle

--- bybitapi.py
+++ bybitapi.py
@@ -1,4 +1,5 @@
 import bybit
+import sys
 from decouple import config
 
 API_KEY = config("API_KEY")
@@ -9,3 +10,4 @@ try:
     print("Logged in successfully!")
 except Exception as e:
     print(f'Something went wrong while logging => {e}')
+    sys.exit()
