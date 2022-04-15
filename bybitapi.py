9cf2f45de702375997ab720fe79b3137565a743d
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-04-15 22:29:18 +0200

    fix initiate connection and update data funcs

--- bybitapi.py
+++ bybitapi.py
@@ -8,24 +8,20 @@ API_SECRET = config("API_SECRET")
 
 try:
     client = bybit.bybit(test=False, api_key=API_KEY, api_secret=API_SECRET)
-    info = client.Market.Market_symbolInfo().result()
-    res = info[0]['result']
-    print("Logged in successfully!")
 except Exception as e:
     print(f'Something went wrong while logging => {e}')
     sys.exit()
 
 
-def updateInfo():
-    while True:
-        updatedInfo = client.Market.Market_symbolInfo().result()
-        response = updatedInfo[0]['result']
-        time.sleep(300)
+def updateData():
+    info = client.Market.Market_symbolInfo().result()
+    return info[0]['result']
 
 
 def getPairApi(pair):
     foundPair = None
-    for item in res:
+    data = updateData()
+    for item in data:
         if item['symbol'] == pair:
             foundPair = item
     return foundPair
