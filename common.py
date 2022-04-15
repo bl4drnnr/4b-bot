79c4f04992c0ebaf47ed92e584e2b3589a91d7db
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-04-15 21:11:52 +0200

    get pair func

--- common.py
+++ common.py
@@ -1,2 +1,14 @@
 def getMostPopularPairs():
     return ["BTCUSDT", "ETHUSDT", "LTCUSDT", "BNBUSDT"]
+
+
+def printPairResult(pair):
+    return f'Here you go with <b>{pair["symbol"]}</b> pair:\n\n' \
+           f'Mark price: <b>{pair["mark_price"]}</b>\n' \
+           f'Index price: <b>{pair["index_price"]}</b>\n\n' \
+           f'Purchase price: <b>{pair["bid_price"]}</b>\n' \
+           f'Selling price: <b>{pair["ask_price"]}</b>\n\n' \
+           f'The highest price in the last 24 hours: <b>{pair["high_price_24h"]}</b>\n' \
+           f'Lowest price in the last 24 hours: <b>{pair["low_price_24h"]}</b>\n\n' \
+           f'<a>/menu</a>\n' \
+           f'<a>/getpair</a>'
