4fc5726acca813ff3f5910ba1a439702b49c39c5
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-04-16 20:26:35 +0200

    dockerfile fixes

--- services/database.py
+++ services/database.py
@@ -6,7 +6,7 @@ DB_DOTENV_FILE = str(os.getcwd()) + '/db.env'
 env_config = Config(RepositoryEnv(DB_DOTENV_FILE))
 
 db = mysql.connector.connect(
-    host="127.0.0.1",
+    host="pcmdb",
     user="root",
     password=env_config.get("MYSQL_PASSWORD"),
     database=env_config.get("MYSQL_DATABASE"),
