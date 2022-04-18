6694abac626764d389b9f148d79b8f6b7bea30d2
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-04-18 11:52:40 +0200

    fix db host

--- services/database.py
+++ services/database.py
@@ -6,7 +6,7 @@ DB_DOTENV_FILE = str(os.getcwd()) + '/db.env'
 env_config = Config(RepositoryEnv(DB_DOTENV_FILE))
 
 db = mysql.connector.connect(
-    host=(env_config.get("PROD_HOST"), env_config.get("DEV_HOST"))[env_config.get("APP_ENV") == 'development'],
+    host=env_config.get("MYSQL_HOST"),
     user="root",
     password=env_config.get("MYSQL_PASSWORD"),
     database=env_config.get("MYSQL_DATABASE"),
