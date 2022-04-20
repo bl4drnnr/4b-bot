4760aec99d6dd860f5d77221d901f70deced0e84
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-04-20 20:51:32 +0200

    fixes

--- services/thread.py
+++ services/thread.py
@@ -4,7 +4,7 @@ import time
 exitFlag = 0
 
 
-class myThread (threading.Thread):
+class Thread(threading.Thread):
     def __init__(self, threadID, name, counter):
         threading.Thread.__init__(self)
         self.threadID = threadID
@@ -26,9 +26,8 @@ def print_time(threadName, counter, delay):
         counter -= 1
 
 
-# Create new threads
-thread1 = myThread(1, "Thread-1", 1)
-thread2 = myThread(2, "Thread-2", 2)
+thread1 = Thread(1, "Thread-1", 1)
+thread2 = Thread(2, "Thread-2", 2)
 
 # Start new Threads
 thread1.start()
