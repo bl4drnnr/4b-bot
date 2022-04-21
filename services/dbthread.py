42290459ae595c7a92b951f3def1ca42531b5729
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-04-21 20:57:33 +0200

    dbthread basics

--- services/dbthread.py
+++ services/dbthread.py
@@ -1,14 +1,31 @@
 import threading
 import time
 
+from services.database import getAllTriggeredAlarms
+
+IDX = 0
+
 
 class DataBaseThread(threading.Thread):
-    def __init__(self):
+    def __init__(self, threadID, chatid):
         threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.chatid = chatid
 
     def run(self):
-        checkForTriggeredAlarmsInDb()
+        checkForTriggeredAlarmsInDb(self.chatid)
 
 
-def checkForTriggeredAlarmsInDb():
+def checkForTriggeredAlarmsInDb(chatid):
     return None
+
+
+def startForCheckingForTriggeredAlarm():
+    dbThread = DataBaseThread(0, 'chatid')
+    increaseIdx()
+    dbThread.start()
+
+
+def increaseIdx():
+    global IDX
+    IDX += 1
