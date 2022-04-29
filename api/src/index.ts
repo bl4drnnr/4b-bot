24b6bec111662b79557fa8e024e45b5c7384e0d4
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-04-29 14:53:04 +0200

    path env fix

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import routes from './routes';
+import path from 'path';
+
+dotenv.config({
+    path: path.resolve(__dirname, '../../.env')
+});
 
 const api = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 api.use('/', routes);
-
-dotenv.config();
 
 api.listen(PORT, () => {
     console.log(`API is listening on port ${PORT}`)
