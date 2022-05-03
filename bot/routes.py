1de44742a0ddcc54c48640f53428be716220ec9c
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-05-03 16:24:18 +0200

    python routes

--- bot/routes.py
+++ bot/routes.py
@@ -1,21 +1,28 @@
 import requests
-# User ID should be hidden
 import crypt
 
-def getUserAlarmsById(id):
-    return
-
-def createAlarm():
-    return
+URL="http://localhost:3000"
 
 def getUserById(id):
-    return
+    r = requests.get(url=URL + '/user/' + id)
+    return r.json()
 
 def createUser():
-    return
+    r = requests.post(url=URL + '/user/create', data=data)
+    return r.json()
+
+def getUserAlarmsById(id):
+    r = requests.get(url=URL + '/alarms/' + id)
+    return r.json()
+
+def createAlarm(data):
+    r = requests.post(url=URL + '/alarm/create', data=data)
+    return r.json()
 
 def getUserPositionsById(id):
-    return
+    r = requests.get(url=URL + '/positions/' + id)
+    return r.json()
 
-def createPosition():
-    return
+def createPosition(data):
+    r = requests.post(url=URL + '/positions/create', data=data)
+    return r.json()
