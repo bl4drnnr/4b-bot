42802b275da6be35df95e0bf704072169d42261c
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-05-04 20:25:33 +0200

    user services

--- api/controllers/user.controller.ts
+++ api/controllers/user.controller.ts
@@ -3,20 +3,24 @@ import * as userService from '../services/user.service';
 
 class UserController {
     async create(req: Request, res: Response) {
-        const user = await userService.createUser(req.body)
-        return res.json(user)
+        const user = await userService.createUser(req.body);
+        return res.json(user);
     }
     async read(req: Request, res: Response) {
-        const { id } = req.params
-        const user = await userService.getUserById(id)
-        return res.json(user)
+        const { id } = req.params;
+        const user = await userService.getUserById(id);
+        return res.json(user);
     }
     async update(req: Request, res: Response) {
-        const { id } = req.params
+        const { id } = req.params;
+        const user = await userService.updateUser(id, req.body);
+        return res.json(user);
     }
     async delete(req: Request, res: Response) {
-        const { id } = req.params
+        const { id } = req.params;
+        const user = await userService.deleteUser(id);
+        return res.json(user);
     }
-}
+};
 
 export { UserController }
--- api/repositories/user.repository.ts
+++ api/repositories/user.repository.ts
@@ -1,9 +1,18 @@
-const knex = require('../src/db/knex.js')
+const knex = require('../src/db/knex.js');
+const uuid = require('uuid');
 
 export const getUserById = async (id: string) => {
-    return await knex('test').select('*')
+    return await knex('test').where('userid', id).first();
 };
 
 export const createUser = async (data: object) => {
-    return await knex('users').insert(data)
+    return await knex('users').insert(data);
+};
+
+export const updateUser = async (id: string, data: object) => {
+    return await knex('users').update(data).where('userid', id);
+};
+
+export const deleteUser = async (id: string) => {
+    return await knex('users').del().where('userid', id);
 };
