176547bdc4cef29207492a022b5a5a557f4d8fdb
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-05-04 22:41:30 +0200

    alarm controller update

--- api/controllers/alarm.controller.ts
+++ api/controllers/alarm.controller.ts
@@ -1,26 +1,25 @@
 import { Request, Response } from "express";
 import * as alarmSerivce from '../services/alarm.service';
 
-class AlarmController {
-    async create(req: Request, res: Response) {
-        const alarm = await alarmSerivce.createAlarm(req.body);
-        return res.json(alarm);
-    }
-    async read(req: Request, res: Response) {
-        const { id } = req.params;
-        const alarm = await alarmSerivce.getUserAlarmsById(id);
-        return res.json(alarm);
-    }
-    async update(req: Request, res: Response) {
-        const { id } = req.params;
-        const alarm = await alarmSerivce.updateAlarm(id, req.body);
-        return res.json(alarm);
-    }
-    async delete(req: Request, res: Response) {
-        const { id } = req.params;
-        const alarm = await alarmSerivce.deleteAlarm(id);
-        return res.json(alarm);
-    }
-}
+export const createAlarm = async (req: Request, res: Response) => {
+    const alarm = await alarmSerivce.createAlarm(req.body);
+    return res.json(alarm);
+};
 
-export { AlarmController }
+export const getUserAlarmsById = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const alarm = await alarmSerivce.getUserAlarmsById(id);
+    return res.json(alarm);
+};
+
+export const updateAlarm = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const alarm = await alarmSerivce.updateAlarm(id, req.body);
+    return res.json(alarm);
+};
+
+export const deleteAlarm = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const alarm = await alarmSerivce.deleteAlarm(id);
+    return res.json(alarm);
+};
--- api/src/routes.ts
+++ api/src/routes.ts
@@ -3,21 +3,20 @@ import { Router } from "express";
 const router = Router();
 
 import { UserController } from '../controllers/user.controller';
-import { AlarmController } from '../controllers/alarm.controller';
+import * as alarmController from '../controllers/alarm.controller';
 import * as positionController from '../controllers/position.controller';
 
 const userController = new UserController();
-const alarmController = new AlarmController();
 
 router.get('/user/:id', userController.read);
 router.post('/user/create', userController.create);
 router.put('/user/update/:id', userController.update);
 router.delete('/user/delete/:id', userController.delete);
 
-router.get('/alarms/:id', alarmController.read);
-router.post('/alarm/create', alarmController.create);
-router.put('/alarm/update/:id', alarmController.update);
-router.delete('/alarm/delete/:id', alarmController.delete);
+router.get('/alarms/:id', alarmController.getUserAlarmsById);
+router.post('/alarm/create', alarmController.createAlarm);
+router.put('/alarm/update/:id', alarmController.updateAlarm);
+router.delete('/alarm/delete/:id', alarmController.deleteAlarm);
 
 router.get('/positions/:id', positionController.getUserPositionsById);
 router.post('/positions/create', positionController.createPosition);
