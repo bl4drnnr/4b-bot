d326b7e0fc6b335c2b58277174191e0c6e04c326
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-05-05 18:21:28 +0200

    position controller logger

--- api/controllers/position.controller.ts
+++ api/controllers/position.controller.ts
@@ -1,25 +1,60 @@
 import { Request, Response } from "express";
 import * as positionService from '../services/position.service';
+import loggerConfig from "../common/logger";
+
+const logger = loggerConfig({ label: 'position-controller', path: 'position' })
 
 export const createPosition = async (req: Request, res: Response) => {
-    const position = await positionService.createPosition(req.body);
-    return res.json(position);
+    try {
+        const position = await positionService.createPosition(req.body);
+
+        logger.info(`Creating position: ${req.body}`);
+
+        return res.json(position);
+    } catch (e) {
+        logger.error(`Error whilte creating position => ${e}`);
+        return res.json({ status: -1 });
+    }
 };
 
 export const getUserPositionsById = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const position = await positionService.getUserPositionsById(id);
-    return res.json(position);
+    try {
+        const { id } = req.params;
+
+        logger.info(`Getting user postioins by id: ${id}`);
+
+        const position = await positionService.getUserPositionsById(id);
+        return res.json(position);
+    } catch (e) {
+        logger.error(`Error white getting user positions => ${e}`);
+        return res.json({ status: -1 });
+    }
 };
 
 export const updatePosition = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const position = await positionService.updatePosition(id, req.body);
-    return res.json(position);
+    try {
+        const { id } = req.params;
+
+        logger.info(`Updating position by id: ${id}`);
+
+        const position = await positionService.updatePosition(id, req.body);
+        return res.json(position);
+    } catch (e) {
+        logger.error(`Error white updating user positions => ${e}`);
+        return res.json({ status: -1 });
+    }
 };
 
 export const deletePosition = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const position = await positionService.deletePosition(id);
-    return res.json(position);
+    try {
+        const { id } = req.params;
+
+        logger.info(`Deleting position by id: ${id}`);
+
+        const position = await positionService.deletePosition(id);
+        return res.json(position);
+    } catch (e) {
+        logger.error(`Error while deleting position => ${e}`);
+        return res.json({ status: -1 })
+    }
 };
