4c629a5aaabf78849b28c1bfe23334bc98264f2a
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-05-07 19:29:27 +0200

    fix return crypto controller

--- api/controllers/crypto.controller.ts
+++ api/controllers/crypto.controller.ts
@@ -7,7 +7,7 @@ const logger = loggerConfig({ label: 'crypto-controller', path: 'crypto' })
 export const getPair = async (req: Request, res: Response) => {
     try {
         const { pair } = req.params;
-        return await cryptoService.getPair(pair);
+        return res.json(pair);
     } catch (e) {
         logger.error(`Error while getting pair => ${e}`);
         return res.json({ status: -1 });
