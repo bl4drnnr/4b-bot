a781971cf3e26fd24f1c0dc472027ba23e3dda5d
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-05-07 18:21:28 +0200

    fixes

--- bot/routes.py
+++ bot/routes.py
@@ -1,33 +1,40 @@
 import requests
 import json
-import crypt
 
 URL="http://localhost:3000"
+
 
 def getUserById(id):
     r = requests.get(url=URL + '/user/' + str(id))
     return r.json()
 
+
 def createUser(data):
     r = requests.post(url=URL + '/user/create', data=data)
     return r.json()
+
 
 def getUserAlarmsById(id):
     r = requests.get(url=URL + '/alarms/' + str(id))
     return r.json()
 
+
 def createAlarm(data):
     r = requests.post(url=URL + '/alarm/create', data=data)
     return r.json()
+
 
 def getUserPositionsById(id):
     r = requests.get(url=URL + '/positions/' + str(id))
     return r.json()
 
+
 def createPosition(data):
     r = requests.post(url=URL + '/positions/create', data=data)
     return r.json()
 
+
 def getPair(pair):
     r = requests.get(url=URL + '/crypto/pair/' + str(pair))
     return r.json()
+
