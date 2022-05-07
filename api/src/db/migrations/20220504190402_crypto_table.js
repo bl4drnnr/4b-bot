90b5660b471adba2be444aad8c3502afa0ede03a
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-05-07 19:31:32 +0200

    migration fix

--- api/src/db/migrations/20220504190402_crypto_table.js
+++ api/src/db/migrations/20220504190402_crypto_table.js
@@ -5,8 +5,13 @@
 exports.up = function(knex) {
   return knex.schema.createTable("crypto", t => {
       t.uuid("id").primary().notNullable()
-      t.string("pair")
-      t.float("currentPrice")
+      t.string("symbol")
+      t.float("mark_price")
+      t.float("index_price")
+      t.float("bid_price")
+      t.float("ask_price")
+      t.float("high_price_24h")
+      t.float("low_price_24h")
       t.timestamp('createdAt').defaultTo(knex.fn.now())
       t.timestamp('updatedAt').defaultTo(knex.fn.now())
   })
