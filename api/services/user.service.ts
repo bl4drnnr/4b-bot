dce1bfa04ef4d53cbb3a0e66a35eb051cd1bd42d
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-05-19 09:47:12 +0200

    remove unused services

--- api/services/user.service.ts
+++ api/services/user.service.ts
@@ -26,37 +26,3 @@ export const createUser = async (user: IUser) => {
         throw Error("error-while-creating-user");
     }
 };
-
-export const updateUser = async (user: IUser) => {
-    try {
-        logger.info(`Check while creating if user with this id already exists: ${user.userid}`)
-        const foundUser = await getUserById(user.userid);
-
-        if (!foundUser) {
-            logger.warn(`User with id: ${user.userid} doesn't exists`)
-            return
-        }
-
-        return await userRepository.updateUser(user);
-    } catch (error: any) {
-        logger.error(`error-while-updating-user => ${error}`);
-        throw Error("error-while-updating-user");
-    }
-};
-
-export const deleteUser = async (user: IUser) => {
-    try {
-        logger.info(`Check while creating if user with this id already exists: ${user.userid}`)
-        const foundUser = await getUserById(user.userid);
-
-        if (!foundUser) {
-            logger.warn(`User with id: ${user.userid} doesn't exists`)
-            return
-        }
-
-        return await userRepository.deleteUser(user);
-    } catch (error: any) {
-        logger.error(`error-while-deleting-user => ${error}`);
-        throw Error("error-while-deleting-user");
-    }
-};
