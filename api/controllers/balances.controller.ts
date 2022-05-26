b21f07845b0855ddc767383c13a979b1a47abbb2
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-05-26 16:32:40 +0200

    encrypting refactor

--- api/controllers/balances.controller.ts
+++ api/controllers/balances.controller.ts
@@ -1,11 +1,13 @@
 import { Request, Response } from "express";
 import * as balancesService from "../services/balances.service";
+import * as securityService from "../services/security.service";
 import loggerConfig from "../common/logger";
 
 const logger = loggerConfig({ label: "balances-controller", path: "balances" });
 
 export const getClientBalancesById = async (req: Request, res: Response) => {
     try {
+        const encryptedId = securityService.encrypt(req.params.id)
         return balancesService.getClientBalancesById(req.params.id);
     } catch (e) {
         logger.error(`Error while getting clients balances => ${e}`);
--- api/services/security.service.ts
+++ api/services/security.service.ts
@@ -1,6 +1,17 @@
 import * as crypto from "crypto";
+import dotenv from "dotenv";
+import path from "path";
 
-export const encrypt = (text: string, key: Buffer, iv: Buffer) => {
+dotenv.config({
+    path: path.resolve(__dirname, "../.env")
+});
+
+// @ts-ignore
+const key = Buffer.from(process.env.CRYPTO_KEY, "hex");
+// @ts-ignore
+const iv = Buffer.from(process.env.IV, "hex");
+
+export const encrypt = (text: string) => {
     const cipher = crypto.createCipheriv("aes-256-cbc", key, iv);
     let result = cipher.update(text, "utf8", "hex");
     result += cipher.final("hex");
@@ -8,7 +19,7 @@ export const encrypt = (text: string, key: Buffer, iv: Buffer) => {
     return result;
 };
 
-export const decrypt = (text: string, key: Buffer, iv: Buffer) => {
+export const decrypt = (text: string) => {
     const decipher = crypto.createDecipheriv("aes-256-cbc", key, iv);
     let result = decipher.update(text, "hex", "utf8");
     result += decipher.final("utf8");
