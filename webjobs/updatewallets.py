64d9f1a369a5724ae86d3f0f8dfc4984a3d3a356
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-05-29 12:58:38 +0200

    updatewallets

--- webjobs/updatewallets.py
+++ webjobs/updatewallets.py
@@ -1,1 +1,15 @@
 import blockcypher
+import requests
+
+from decouple import config
+
+URL="http://127.0.0.1:3000"
+BASIC_USERNAME = config('BASIC_USERNAME')
+BASIC_PASSWORD = config('BASIC_PASSWORD')
+
+
+def updateWallets():
+    return
+
+
+updateWallets()
